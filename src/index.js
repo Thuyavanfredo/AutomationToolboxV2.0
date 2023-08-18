85e3ff523ff48b7e66b74546a4e98aaa24289a81
Thuyavanfredo <thuyavan.tamilarasan@gmail.com>
2023-08-19 03:55:38 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const {autoUpdater}=require("electron-updater")
+const log = require('electron-log');
+const fs = require("fs")
+
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
+fs.mkdir("C:/Automation Toolbox", function(err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log("New directory successfully created.")
+  }
+})
+log.transports.file.resolvePath = () => path.join("C:/Automation Toolbox", '/logs/main.log');
+log.log("Application version"+ app.getVersion())
+log.info('Hello, log');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// eslint-disable-next-line global-require
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
-
+function handleSetTitle (event, title) {
+  var child = require('child_process').execFile;
+  var executablePath = "./resources/Effectivity Macro 2.06.exe";
+  
+  child(executablePath, function(err, data) {
+      if(err){
+         console.error(err);
+         return;
+      }
+   
+      console.log(data.toString());
+  });
+}
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -24,6 +54,7 @@ const createWindow = () => {
     },
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: true,
       // enableRemoteModule: true,
       preload: path.join(__dirname, 'preload.js'),
     },
@@ -41,7 +72,11 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  ipcMain.on('set-title', handleSetTitle)
+  createWindow()
+  autoUpdater.checkForUpdatesAndNotify()
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -57,8 +92,32 @@ app.on('activate', () => {
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
+    autoUpdater.checkForUpdatesAndNotify()
   }
 });
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+autoUpdater.on("update-available",()=>{
+log.info("Update available")
+})
+autoUpdater.on("checking-for-update",()=>{
+  log.info("checking-for-update")
+})
+  autoUpdater.on("download-progress",(progressTrack)=>{
+    log.info("\n\ndownload-progress")
+  log.info(progressTrack)
+})
+autoUpdater.on("update-downloaded",()=>{
+  log.info("update-downloaded")
+})
+autoUpdater.on("update-not-available",()=>{
+  log.info("update-not-available")
+})
+autoUpdater.on("error",(err)=>{
+  log.info("Error", err)
+})
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,60 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer } = require('electron')
+const { writeFile, readFile } = require("fs");
+contextBridge.exposeInMainWorld('electronAPI', {
+    setTitle: (title) => ipcRenderer.send('set-title', title),
+    updateEvent:(buttonType)=>{
+
+        // const networkSharePath = '\\catedcnas2.ap.cat.com\PSID-I\Automation analytics';
+
+        // Replace 'file.txt' with the desired file name
+        // const fileName = 'process.env.USERNAME+".json';
+
+        // Full path of the file on the network share drive
+        // const filePath = path.join(networkSharePath, fileName);
+
+        // const path = "G:/Analytics/"+process.env.USERNAME+".json";
+        const path = "//catedcnas2.ap.cat.com/PSID-I/Analytics/"+process.env.USERNAME+".json";
+    
+        readFile(path, (error, data) => {
+            if (error) {
+                // console.log(error);
+                    writeFile(path, JSON.stringify([{
+                        userName: process.env.USERNAME,
+                        buttonType,
+                    }], null, 2), (err) => {
+                        if (err) {
+                            console.log("Failed to write updated data to file");
+                            return;
+                        }
+                        console.log("Updated file successfully");
+                    });
+                return;
+            }
+        })
+        readFile(path, (error, data) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            console.log(data)
+            
+            const parsedData = JSON.parse(data);
+            
+            // updating name in shipping_address
+            parsedData.push({
+                userName: process.env.USERNAME,
+                buttonType,
+            });
+            
+            writeFile(path, JSON.stringify(parsedData, null, 2), (err) => {
+                if (err) {
+                    console.log("Failed to write updated data to file");
+                    return;
+                }
+                console.log("Updated file successfully");
+            });
+        });
+    }
+})
