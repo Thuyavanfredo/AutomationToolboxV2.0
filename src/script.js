97a2e7f6c550532ff077dd7008705e763e7ed7bc
Thuyavanfredo <thuyavan.tamilarasan@gmail.com>
2023-08-18 15:23:05 +0530

    new

--- src/script.js
+++ src/script.js
@@ -694,5 +694,44 @@ $(document).ready(function () {
     $(".da-btns").hide();
     $(".video").hide();
     });
-  });
+    $("#pdt").click(function () {
+      $(".body").html("");
+      $(".body").css({
+          "background-image": "none",
+          "width": "625px",
+          "transition":"all 0.5s linear",
+          "transform": "translateX(180px)"
+      });
+      $("video").hide();
+      $(".pdt-btns").show();
+      $(".spec-btns").hide();
+      $(".da-btns").hide();
+    });
+  $("#spec").click(function () {
+      $(".body").html("");
+      $(".body").css({
+          "background-image": "none",
+          "width": "625px",
+          "transition":"all 0.5s linear",
+          "transform": "translateX(180px)"
+      });
+      $("video").hide();
+      $(".pdt-btns").hide();
+      $(".spec-btns").show();
+      $(".da-btns").hide();
+    });
+  $("#da").click(function () {
+      $(".body").html("");
+      $(".body").css({
+          "background-image": "none",
+          "width": "625px",
+          "transition":"all 0.5s linear",
+          "transform": "translateX(180px)"
+      });
+      $("video").hide();
+      $(".pdt-btns").hide();
+      $(".spec-btns").hide();
+      $(".da-btns").show();
+    });
+});
   
